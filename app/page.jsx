45cbf2a1c5dd5014b074411182f16e63295323be
MurAd <muradisayevazer@gmail.com>
2023-10-29 16:55:49 +0400

    sub components were created

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
+import TicketCard from "./(components)/TicketCard";
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="p-5">
+      <div className="grid-cols-2 md:grid lg:grid-cols-3">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
   )
 }
 
