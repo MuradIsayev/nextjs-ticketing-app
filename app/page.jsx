3bc120c9f719a8364be0dd3ab540aa42fbcacab1
MurAd <muradisayevazer@gmail.com>
2023-10-31 02:15:06 +0400

    Dashboard view was updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,37 @@
 import TicketCard from "./(components)/TicketCard";
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch('http://localhost:3000/api/Tickets', {
+      cache: 'no-store'
+    })
+
+    return res.json();
+  } catch (error) {
+    console.log('Failed to get Tickets', error);
+  }
+}
+
+const Dashboard = async () => {
+  const { tickets } = await getTickets()
+
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category))
+  ];
+
   return (
     <div className="p-5">
-      <div className="grid-cols-2 md:grid lg:grid-cols-3">
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+          <div className="mb-4" key={categoryIndex}>
+            <h3>{uniqueCategory}</h3>
+            <div className="grid-cols-2 md:grid lg:grid-cols-3">
+              {tickets.filter((ticket) => ticket.category === uniqueCategory).map((filteredTicket, _index) => (
+                <TicketCard id={_index} key={_index} ticket={filteredTicket} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
