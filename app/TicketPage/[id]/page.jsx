7c0bd7d39565dd5c273242ad7e43f9f281647ba9
MurAd <muradisayevazer@gmail.com>
2023-10-31 15:18:14 +0400

    Ticket Page was updated

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,31 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
-const TicketPage = ({ params }) => {
-    return <TicketForm />
+const getTicketById = async (id) => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+        cache: 'no-store'
+    })
+
+    if (!res.ok) {
+        throw new Error("Failed to get Ticket.")
+    }
+
+    return res.json();
+}
+
+const TicketPage = async ({ params }) => {
+    const EDIT_MODE = params.id === 'new' ? false : true;
+    let updateTicketData = {}
+
+    if (EDIT_MODE) {
+        updateTicketData = await getTicketById(params.id);
+        updateTicketData = updateTicketData.foundTicket;
+    } else {
+        updateTicketData = {
+            _id: 'new'
+        }
+    }
+
+    return <TicketForm ticket={updateTicketData} />
 
 }
 
